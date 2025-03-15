3e7e799a1c4ce5c5942808a647fc308d3abc2bcf
PortiaMasibi <portia.masibi@mail.mcgill.ca>
2025-03-14 20:47:26 -0400

    building game and testing it

---
+++ letsPlay/TestClass.java
@@ -0,0 +1,10 @@
+package letsPlay;
+
+public class TestClass {
+    public static void main(String[] args) throws InvalidMoveException, InputMismatchException, ColumnFullException {
+        //creating a new instance of the Game class
+        Game game = new Game();
+        //calling the play method
+        game.playe();
+    }
+}
